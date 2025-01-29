50e7be67d2f0350766e3e95512ef531b3ab2ae97
DE COLIGNY Thibault <thibault.2coligny@gmail.com>
2025-01-29 08:32:59 +0100

    Modification du build.yml, et deployment.yml

---
+++ src/main/java/fr/efrei/spring_rag/service/DocumentService.java
@@ -0,0 +1,33 @@
+package fr.efrei.spring_rag.service;
+
+import fr.efrei.spring_rag.domain.Document;
+import fr.efrei.spring_rag.repository.DocumentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DocumentService {
+    private final DocumentRepository documentRepository;
+
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    public Document buildAndSaveDocument(Document document) {
+        return documentRepository.save(document);
+    }
+
+    public Optional<Document> findById(Long id) {
+        return documentRepository.findById(id);
+    }
+
+    public List<Document> findAll() {
+        return documentRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        documentRepository.deleteById(id);
+    }
+}
